eb85384a037a061a15e6a168aead3e075ec0e734
Perf2k2 <perf2k2@gmail.com>
2019-06-13 19:01:43 +0300

    Password authenticator implementation

--- src/Authenticators/PasswordAuthenticator.php
+++ src/Authenticators/PasswordAuthenticator.php
@@ -7,9 +7,17 @@ use Perf2k2\Remmoit\AuthenticatorInterface;
 
 class PasswordAuthenticator implements AuthenticatorInterface
 {
+    private $user;
+    private $password;
+
+    public function __construct($user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
 
     public function auth($connection)
     {
-
+        ssh2_auth_password($connection, $this->user, $this->password);
     }
 }
