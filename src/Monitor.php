52d59cb8e22a04817fce91fa05ca810e0d06687b
Perf2k2 <perf2k2@gmail.com>
2019-06-13 19:01:08 +0300

    Connecting to server

--- src/Monitor.php
+++ src/Monitor.php
@@ -5,15 +5,29 @@ namespace Perf2k2\Remmoit;
 
 class Monitor
 {
+    /**
+     * @var Server[]
+     */
     private $servers = [];
+    private $checkers = [];
 
-    public function addChecks(Server $server, array $checks): self
+    public function addChecks(Server $server, array $checkers): self
     {
+        if (!array_key_exists($server->getAddress(), $this->servers)) {
+            $this->servers[$server->getAddress()] = $server;
+        }
+
+        $this->servers[$server->getAddress()] = $server;
+        $this->checkers[$server->getAddress()] = $checkers;
+
         return $this;
     }
 
     public function run(): void
     {
-
+        foreach ($this->servers as $server) {
+            $connection = ssh2_connect($server->getAddress(), $server->getPort());
+            $server->getAuthenticator()->auth($connection);
+        }
     }
 }
--- src/Server.php
+++ src/Server.php
@@ -5,8 +5,29 @@ namespace Perf2k2\Remmoit;
 
 class Server
 {
-    public function __construct(AuthenticatorInterface $authenticator)
-    {
+    private $address;
+    private $authenticator;
+    private $port;
 
+    public function __construct(string $address, AuthenticatorInterface $authenticator, int $port = 22)
+    {
+        $this->address = $address;
+        $this->authenticator = $authenticator;
+        $this->port = $port;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function getAuthenticator(): AuthenticatorInterface
+    {
+        return $this->authenticator;
+    }
+
+    public function getPort(): int
+    {
+        return $this->port;
     }
 }
